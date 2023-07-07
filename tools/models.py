1cc97d3fd1f84ced2146283297df9ea81dba825b
vdtheone <vishalprajapati22052001@gmail.com>
2023-07-07 12:10:44 +0530

    last commit

--- tools/models.py
+++ tools/models.py
@@ -6,8 +6,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=25)
-    city = models.CharField(max_length=15,null=True)
-    zip = models.IntegerField(null=True, blank=True)
 
 
 class Product(models.Model):
@@ -20,6 +18,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=False,related_name='address')
+    country = models.CharField(max_length=25)
+    city = models.CharField(max_length=25,null=True)
+    street = models.CharField(max_length=35,null=True)
+    building = models.CharField(max_length=25,null=True)
+    apartment = models.CharField(max_length=25,null=True)
+    floor = models.CharField(max_length=25,null=True)
+    postal_code = models.IntegerField(null=True, blank=True)
+    default_add = models.BooleanField(null=False,default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def make_default(self):
+        self.default_add = True
+
+
+    def __str__(self):
+        return self.city
+
 
 
 class Order(models.Model):
@@ -28,6 +48,8 @@ class Order(models.Model):
     price = models.IntegerField(default=0,blank=True,null=True)
     quantity = models.IntegerField(default=0,blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, null=False, related_name='orders')
+
 
     def __str__(self):
         return self.product.name
@@ -36,9 +58,34 @@ class Order(models.Model):
 class Inventory(models.Model):
     quantity = models.PositiveIntegerField()
     product = models.ForeignKey(Product,on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     # def __str__(self):
     #     return self.title
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
+
+
+class Cart(models.Model):
+    user =  models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
+
+
+
+    
+
 
 
 
--- tools/views.py
+++ tools/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login,logout
-from tools.models import Product,Order,User
+from tools.models import Address, Product,Order,User,Wishlist,Cart
+from reportlab.pdfgen import canvas  
 
 
 
@@ -74,28 +75,20 @@ def product_view(request,id):
     return render(request,'tools/product_view.html',{'product':product})
 
 
-def profile(request,id):
+def profile(request):
     no_of_ord = Order.objects.filter(user=request.user)
+    no_of_wished_item = Wishlist.objects.filter(user=request.user)
     print("No of order == ",no_of_ord)
-    return render(request,'tools/profile.html',{'no_of_ord':no_of_ord})
+    return render(request,'tools/profile.html',{'no_of_ord':no_of_ord,'no_of_wished_item':no_of_wished_item})
 
 
 def place_order(request,id):
     if(request.method == "POST"):
+        all_address = Address.objects.filter(user = request.user)
         product_name = Product.objects.filter(id=id).first()
         hidden_quantity = request.POST.get('hidden_quantity')
         price = request.POST.get('hidden_price')
-        print(product_name)
-        # print(price)
-        # print(hidden_quantity)
-        # print(request.POST)
-        # details = {
-        #     'product_name':product_name,
-        #     'price':price,
-        #     'hidden_quantity':hidden_quantity,
-        #     'id':id
-        # }
-    return render(request,'tools/place_order.html',{'product_name':product_name,'hidden_quantity':hidden_quantity,'price':price})
+    return render(request,'tools/place_order.html',{'product_name':product_name,'hidden_quantity':hidden_quantity,'price':price,'myaddress':all_address})
 
 
 
@@ -104,33 +97,142 @@ def order_enrty(request,id):
         selected_product = Product.objects.get(id=id)
         price = request.POST.get('price')
         quantity = request.POST.get('quantity')
-
-        print('========================================',request.user)
-        print(price)
-        print(selected_product)
-        print(request)
+        select_address = request.POST.get('select_address')
+        print("select_address ========================== ",select_address)
+        add_instance = Address.objects.get(id=select_address)
     
-        ord = Order.objects.create(product_id = id,user = request.user, price=int(price),quantity=int(quantity))
+    
+        ord = Order.objects.create(address = add_instance,product_id = id,user = request.user, price=int(price),quantity=int(quantity))
         print('------------->>>>>> ',ord)
         ord.save()
     return render(request,'tools/order_place_message.html')
 
 
 
-def myaddress(request,id):
-    return render(request,'tools/my_address.html')
+def myaddress(request):
+    all_address = Address.objects.filter(user = request.user)
+    return render(request,'tools/my_address.html',{'myaddress':all_address})
 
 
-def myorder(request,id):
-    return render(request,'tools/my_order.html')
+def myorder(request):
+    address = Address.objects.filter(user = request.user)
+    myorders = Order.objects.filter(user = request.user)
+
+    test_query = Order.objects.select_related('address').filter(user = request.user)
+
+    return render(request,'tools/my_order.html',{'myorders':myorders,'address':address,'test_query':test_query})
 
 
-def mywishlist(request,id):
-    return render(request,'tools/my_bucket.html')
+def mywishlist(request):
+    my_wishlist = Wishlist.objects.filter(user = request.user)
+    return render(request,'tools/my_bucket.html',{'my_wishlist':my_wishlist})
 
 def mysettings(request,id):
-    return render(request,'tools/my_setting_edit.html')
+    if(request.method == "POST"):
+        user = User.objects.get(id = id)
+        user.first_name = request.POST.get('name')
+        user.email = request.POST.get('email')
+        user.city = request.POST.get('city')
+        user.zip = request.POST.get('zip')
+        user.phone_number = request.POST.get('phone_number')
+        print(user.phone_number)
+        print("User = ",user)
+        user.save()
+    else:
+        user = User.objects.get(id = id)
+    return render(request,'tools/my_setting_edit.html',{'userdetails':user})
+
+
+def add_new_address(request):
+    if (request.method == "POST"):
+        print("called============")
+        country = request.POST.get('country')
+        city = request.POST.get('city')
+        street = request.POST.get('street')
+        building = request.POST.get('building')
+        apartment = request.POST.get('apartment')
+        floor = request.POST.get('floor')
+        postal_code = request.POST.get('postal_code')
+
+        add_new_address = Address.objects.create(user=request.user,country=country,city=city,street=street,building=building,apartment=apartment,floor=floor,postal_code=postal_code)
+        add_new_address.save()
+        return redirect('myaddress')
+    
+    return render(request,'tools/add_new_address.html')
+
+
+def make_add_default(request,id):
+    print(request,id)
+    Address.objects.all().update(default_add=False)
+    Address.objects.filter(id=id).update(default_add=True)
+    return redirect('myaddress')
+
+def edit_address(request,id):
+    if(request.method=="POST"):
+        print("Called")
+        user_address = Address.objects.get(id=id)
+        user_address.country = request.POST.get('country')
+        user_address.city = request.POST.get('city')
+        user_address.street = request.POST.get('street')
+        user_address.building = request.POST.get('building')
+        user_address.apartment = request.POST.get('apartment')
+        user_address.floor = request.POST.get('floor')
+        user_address.postal_code = request.POST.get('postal_code')
+        user_address.save()
+        return redirect('myaddress')
+    else:
+        myaddress = Address.objects.get(id = id)
+    return render(request,'tools/add_new_address.html',{'myaddress':myaddress})
 
 
 
+def delete_address(request,id):
+    user_address = Address.objects.get(id=id)
+    user_address.delete()
+    return redirect('myaddress')
+
+
+def getpdf(request):  
+    response = HttpResponse(content_type='application/pdf')  
+    response['Content-Disposition'] = 'attachment; filename="file.pdf"'  
+    p = canvas.Canvas(response)  
+    p.setFont("Times-Roman", 55)  
+    p.drawString(100,700, "Hello, Javatpoint.")  
+    p.showPage()  
+    p.save()  
+    return response  
+
+def add_to_wishlist(request,id):
+    user = User.objects.get(id = request.user.id)
+    product = Product.objects.get(id=id)
+    add_to_wishlist = Wishlist.objects.create(user=user,product=product)
+    add_to_wishlist.save()
+    return redirect('product_view',id=id)
+
+def mycart(request):
+    products_of_cart = Cart.objects.filter(user = request.user)
+    return render(request,'tools/my_bucket.html',{'products_of_cart':products_of_cart})
+
+
+def delete_wishlist_item(request,id):
+    delete_product = Wishlist.objects.get(id=id)
+    delete_product.delete()
+    return redirect('mywishlist')
+
+def add_to_cart(request,id):
+    user = User.objects.get(id = request.user.id)
+    product = Product.objects.get(id=id)
+    add_to_cart = Cart.objects.create(user=user,product=product)
+    add_to_cart.save()
+    return redirect('product_view',id=id)
+
+def delete_cartitem(request,id):
+    print("====================",id)
+    delete_product = Cart.objects.get(id=id)
+    delete_product.delete()
+    return redirect('mycart')
+
+
+# User Model Fields
+# city, date_joined, email, first_name, groups, id, is_active, is_staff, is_superuser, last_login, last_name, logentry, order, password, phone_number, user_permissions, username, zip
         
